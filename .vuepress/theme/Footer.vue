a2d0cb4dbd32efe12917507c7de6d0485a1307e6
vittominacori <vittoriominacori@gmail.com>
2018-12-22 18:39:03 +0100

    add footer link to home

--- .vuepress/theme/Footer.vue
+++ .vuepress/theme/Footer.vue
@@ -2,6 +2,8 @@
     <b-row>
         <b-col lg="10" offset-lg="1" class="text-center text-white p-0">
             <ul class="list-inline mt-2">
+                <li v-if="this.$page.frontmatter.component !== 'Generator'" class="list-inline-item"><small><router-link class="text-light" to="/">Watch your Token</router-link></small></li>
+                <li v-if="this.$page.frontmatter.component !== 'Generator'" class="list-inline-item">|</li>
                 <li class="list-inline-item">
                     <small>created by <b-link class="text-light" href="https://github.com/vittominacori" target="_blank">@vittominacori</b-link></small>
                 </li>
