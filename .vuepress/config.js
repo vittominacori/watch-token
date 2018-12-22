455f62c6ba773e6d83f3d5d4ade0d1247522c403
vittominacori <vittoriominacori@gmail.com>
2018-12-22 19:50:35 +0100

    updated og graph image

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,9 +7,9 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/watch-token' }],
-    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'Token Watcher | Add your Token to DApp browsers or MetaMask' }],
     ['script', { src: '/assets/js/web3.min.js' }],
   ],
