86203702a6abaf3adea17151f9163872489f5603
vittominacori <vittoriominacori@gmail.com>
2018-12-24 16:04:31 +0100

    fix header link

--- .vuepress/theme/Header.vue
+++ .vuepress/theme/Header.vue
@@ -3,7 +3,7 @@
 
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-        <b-navbar-brand :to="$withBase('/')"><b-img :src="$withBase('/assets/images/base-token.png')" height="24" /> WatchToken</b-navbar-brand>
+        <b-navbar-brand to="/"><b-img :src="$withBase('/assets/images/base-token.png')" height="24" /> WatchToken</b-navbar-brand>
 
         <b-collapse is-nav id="nav_collapse">
             <b-navbar-nav>
