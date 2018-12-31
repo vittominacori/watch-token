ce5adac8654ddb001ddd3672ab103c589dce7c19
vittominacori <vittoriominacori@gmail.com>
2018-12-31 16:10:11 +0100

    fix header level

--- .vuepress/components/Home.vue
+++ .vuepress/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <b-row class="mt-4 p-0">
         <b-col lg="8" offset-lg="2">
-            <b-jumbotron bg-variant="light">
+            <b-jumbotron bg-variant="light" header-level="4">
                 <template slot="header">
                     WatchToken
                 </template>
@@ -10,7 +10,7 @@
                 </template>
                 <hr class="my-4">
                 <p>Create a link for your token or an embeddable widget for your website</p>
-                <b-button size="lg" variant="outline-success" class="mt-2" :to="$withBase('create.html')">Create your link</b-button>
+                <b-button size="lg" variant="outline-success" class="mt-2" :to="$withBase('create.html')">Create page</b-button>
                 <b-button size="lg" variant="outline-primary" class="mt-2" :to="$withBase('detail.html?address=0xB5c64c7d6b374aD738e314eAa7bCC07AEc194B7b&network=mainnet&logo=https%3A%2F%2Fvittominacori.github.io%2Fwatch-token%2Fassets%2Fimages%2Fbase-token.png')">View sample</b-button>
             </b-jumbotron>
         </b-col>
