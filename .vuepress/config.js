d0bbcf1bebd0025fa01c6d6712a893a9991efbfe
vittominacori <vittoriominacori@gmail.com>
2020-11-27 22:08:37 +0100

    feat: check to load layout on ready

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,12 +18,6 @@ module.exports = {
     ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'WatchToken | Create a Widget for your ERC20 Token' }],
     ['script', { src: '/assets/js/web3.min.js' }],
-    ['script',
-      {
-        src: 'https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=en&theme=momh&thirdparty=1&always=1&noGeoIp=1&scrolling=1&hideDetailsBtn=1', // eslint-disable-line max-len
-        defer: true,
-      },
-    ],
   ],
   defaultNetwork: 'mainnet',
   infuraProjectId: 'be402bb729b2472ba828e013468dc626',
--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="page-wrapper main-container" :class="bodyClass">
-        <site-header v-if="!embedded"/>
+        <site-header v-if="loadLayout"/>
         <b-container>
             <transition name="fade" mode="out-in">
                 <component v-if="page" :is="page" :key="page"></component>
             </transition>
         </b-container>
-        <site-footer v-if="!embedded"/>
+        <site-footer v-if="loadLayout"/>
     </div>
 </template>
 <script>
@@ -23,12 +23,23 @@
       SiteHeader,
       SiteFooter,
     },
+    data () {
+      return {
+        loaded: false,
+      };
+    },
+    mounted () {
+      this.loaded = true;
+    },
     computed: {
       page () {
         return this.$page.frontmatter.component || null;
       },
+      loadLayout () {
+        return this.loaded && !this.embedded;
+      },
       bodyClass () {
-        return !this.embedded ? 'aqua-gradient' : '';
+        return this.loadLayout ? 'aqua-gradient' : '';
       },
     },
   };
