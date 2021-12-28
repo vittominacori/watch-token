0aa62a0ffb4279fffb0f359fa3f06e04c922b459
vittominacori <vittoriominacori@gmail.com>
2021-12-28 11:33:59 +0100

    publish: feat: add polygon
    generated from commit abd553e7e7c49ad7c7c785131046b60834d13bcc

---
+++ assets/js/10.181bc0f6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{356:function(e,n,t){"use strict";t.r(n);var l={name:"Footer"},o=t(53),r=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=r.exports}}]);
