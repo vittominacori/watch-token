c44b5324a64f948e60cc5b5385bedcff4f99c075
Vittorio Minacori <vittoriominacori@gmail.com>
2022-04-19 22:46:27 +0200

    publish: feat: update links on smart contracts tools
    generated from commit aaec62a18c41306ad15cfd4d6c4f4b8f7104e922

--- assets/js/10.181bc0f6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{356:function(e,n,t){"use strict";t.r(n);var l={name:"Footer"},o=t(53),r=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=r.exports}}]);
---
+++ assets/js/10.f139da94.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{366:function(e,n,t){"use strict";t.r(n);var l={name:"Footer"},o=t(55),r=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=r.exports}}]);
